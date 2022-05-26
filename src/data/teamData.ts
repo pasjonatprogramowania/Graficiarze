239253ed82590d99d136ec422f016fc1d5f69a3b
PasjonatProgramowania <paw.szew1234@gmail.com>
2022-05-26 20:47:22 +0200

    add object chemat for each team member

--- src/data/teamData.ts
+++ src/data/teamData.ts
@@ -1,11 +1,138 @@
 import DEFAULT_AVATAR from '../img/team/avatar.jpg';
+import DEFAULT_IMG from '../img/team/avatar.jpg';
 import DEFAULT_PORTFOLIO from '../img/portfolio/defaultPortfolio.png';
-const DEFAULT_TEXT = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In nemo odio similique provident at adipisci possimus eveniet enim laborum sit repudiandae molestias, ratione nesciunt quos dignissimos eum fugiat nulla delectus.';
-// const DEFAULT_IMG = require('');
-export const PawelSzewczyk = { name: 'Paweł Szewczyk', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const AsiaZiolczynska = { name: 'Asia Ziółczyńska', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const JustynaChojnacka = { name: 'Justyna Chojnacka', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const OlgaSzlifirczyk = { name: 'Olga Szlifirczyk', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const KrzysztofRafalik = { name: 'Krzysztof Rafalik', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const OparcikGrzegorz = { name: 'Oparcik Grzegorz', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
-export const RobertMikulski = { name: 'Robert Mikulski', avatar: DEFAULT_AVATAR, teamMemberData: { portfolioImg: DEFAULT_PORTFOLIO, aboutText: DEFAULT_TEXT, experienceText: DEFAULT_TEXT, experienceImgs: [], educationText: DEFAULT_TEXT, educationImgs: [], certificateText: DEFAULT_TEXT, certificateImgs: [] } };
+const DEFAULT_TEXT = 'Lorem ipsum dolor sit amet .';
+
+// PawelSzewczyk
+const PAWEL_DESCRIPTION = 'lorem ipsum';
+const PAWEL_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const PAWEL_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const PAWEL_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const PAWEL_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const PAWEL_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const PAWEL_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const PAWEL_SZEWCZYK_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: PAWEL_DESCRIPTION,
+    hobby: PAWEL_HOBBY,
+    experience: PAWEL_EXPERIENCE,
+    education: PAWEL_EDUCATION,
+    skills: PAWEL_SKILLS,
+    certyfiactes: PAWEL_CERTYFICATES,
+    others: PAWEL_OTHERS,
+};
+// AsiaZiolczynska
+const ASIA_DESCRIPTION = 'lorem ipsum';
+const ASIA_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const ASIA_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const ASIA_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const ASIA_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const ASIA_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const ASIA_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const ASIA_ZIOLCZYNSKA_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: ASIA_DESCRIPTION,
+    hobby: ASIA_HOBBY,
+    experience: ASIA_EXPERIENCE,
+    education: ASIA_EDUCATION,
+    skills: ASIA_SKILLS,
+    certyfiactes: ASIA_CERTYFICATES,
+    others: ASIA_OTHERS,
+};
+// JustynaChojnacka
+const JUSTYNA_DESCRIPTION = 'lorem ipsum';
+const JUSTYNA_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const JUSTYNA_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const JUSTYNA_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const JUSTYNA_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const JUSTYNA_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const JUSTYNA_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const JUSTYNA_CHOJNACKA_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: JUSTYNA_DESCRIPTION,
+    hobby: JUSTYNA_HOBBY,
+    experience: JUSTYNA_EXPERIENCE,
+    education: JUSTYNA_EDUCATION,
+    skills: JUSTYNA_SKILLS,
+    certyfiactes: JUSTYNA_CERTYFICATES,
+    others: JUSTYNA_OTHERS,
+};
+// OlgaSzlifirczyk
+const OLGA_DESCRIPTION = 'lorem ipsum';
+const OLGA_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const OLGA_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const OLGA_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const OLGA_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const OLGA_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const OLGA_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const OLGA_SZLIFIRCZYK_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: OLGA_DESCRIPTION,
+    hobby: OLGA_HOBBY,
+    experience: OLGA_EXPERIENCE,
+    education: OLGA_EDUCATION,
+    skills: OLGA_SKILLS,
+    certyfiactes: OLGA_CERTYFICATES,
+    others: OLGA_OTHERS,
+};
+// KrzysztofRafalik
+const KRZYSZTOF_DESCRIPTION = 'lorem ipsum';
+const KRZYSZTOF_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const KRZYSZTOF_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const KRZYSZTOF_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const KRZYSZTOF_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const KRZYSZTOF_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const KRZYSZTOF_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const KRZYSZTOF_RAFALIK_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: KRZYSZTOF_DESCRIPTION,
+    hobby: KRZYSZTOF_HOBBY,
+    experience: KRZYSZTOF_EXPERIENCE,
+    education: KRZYSZTOF_EDUCATION,
+    skills: KRZYSZTOF_SKILLS,
+    certyfiactes: KRZYSZTOF_CERTYFICATES,
+    others: KRZYSZTOF_OTHERS,
+};
+// OparcikGrzegorz
+const GRZEGORZ_DESCRIPTION = 'lorem ipsum';
+const GRZEGORZ_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const GRZEGORZ_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const GRZEGORZ_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const GRZEGORZ_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const GRZEGORZ_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const GRZEGORZ_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const GRZEGORZ_OPARCIK_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: GRZEGORZ_DESCRIPTION,
+    hobby: GRZEGORZ_HOBBY,
+    experience: GRZEGORZ_EXPERIENCE,
+    education: GRZEGORZ_EDUCATION,
+    skills: GRZEGORZ_SKILLS,
+    certyfiactes: GRZEGORZ_CERTYFICATES,
+    others: GRZEGORZ_OTHERS,
+};
+// RobertMikulski
+const ROBERT_DESCRIPTION = 'lorem ipsum';
+const ROBERT_HOBBY = ['lorem ipsum', 'lorem ipsum', 'lorem ipsum'];
+const ROBERT_EXPERIENCE = [{ date: '12.12.2023', text: 'Lorem ipsum dolor sit amet ' }];
+const ROBERT_EDUCATION = 'Zespół Szkół Technicznych im.Tadeusza Kościuszki';
+const ROBERT_SKILLS = [{ name: 'lorem ipsum', description: 'lorem ipsum' }];
+const ROBERT_CERTYFICATES = [{ name: 'lorem ipsum', img: DEFAULT_IMG }];
+const ROBERT_OTHERS = [{ name: 'lorem ipsum', img: 'lorem ipsum' }];
+const ROBERT_MIKULSKI_PORTFOLIO_DATA = {
+    portfolioImg: DEFAULT_PORTFOLIO,
+    description: ROBERT_DESCRIPTION,
+    hobby: ROBERT_HOBBY,
+    experience: ROBERT_EXPERIENCE,
+    education: ROBERT_EDUCATION,
+    skills: ROBERT_SKILLS,
+    certyfiactes: ROBERT_CERTYFICATES,
+    others: ROBERT_OTHERS,
+};
+export const PawelSzewczyk = { name: 'Paweł Szewczyk', avatar: DEFAULT_AVATAR, teamMemberData: PAWEL_SZEWCZYK_PORTFOLIO_DATA };
+export const AsiaZiolczynska = { name: 'Asia Ziółczyńska', avatar: DEFAULT_AVATAR, teamMemberData: ASIA_ZIOLCZYNSKA_PORTFOLIO_DATA };
+export const JustynaChojnacka = { name: 'Justyna Chojnacka', avatar: DEFAULT_AVATAR, teamMemberData: JUSTYNA_CHOJNACKA_PORTFOLIO_DATA };
+export const OlgaSzlifirczyk = { name: 'Olga Szlifirczyk', avatar: DEFAULT_AVATAR, teamMemberData: OLGA_SZLIFIRCZYK_PORTFOLIO_DATA };
+export const KrzysztofRafalik = { name: 'Krzysztof Rafalik', avatar: DEFAULT_AVATAR, teamMemberData: KRZYSZTOF_RAFALIK_PORTFOLIO_DATA };
+export const OparcikGrzegorz = { name: 'Oparcik Grzegorz', avatar: DEFAULT_AVATAR, teamMemberData: GRZEGORZ_OPARCIK_PORTFOLIO_DATA };
+export const RobertMikulski = { name: 'Robert Mikulski', avatar: DEFAULT_AVATAR, teamMemberData: ROBERT_MIKULSKI_PORTFOLIO_DATA };
